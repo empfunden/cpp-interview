281bcb828bdd72778b0d9b43f40cb5b7cff50af4
empfunden <90171851+empfunden@users.noreply.github.com>
2024-11-04 04:29:58 +0000

    Passed all cases

--- src/Exchange.hpp
+++ src/Exchange.hpp
@@ -4,7 +4,6 @@
 #include "Types.hpp"
 
 #include <iostream>
-#include <map>
 #include <algorithm>
 
 namespace aqtc {
--- src/Types.hpp
+++ src/Types.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <map>
+
 namespace aqtc {
 
 using OrderId = unsigned long;
